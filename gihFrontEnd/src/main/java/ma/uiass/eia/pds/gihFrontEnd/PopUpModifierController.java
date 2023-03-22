4d527eeeef558ed79d948f8bc00d68e4c798417b
Oussama <semlali.oussama09@gmail.com>
2023-03-22 19:18:56 +0000

    update3

--- gihFrontEnd/src/main/java/ma/uiass/eia/pds/gihFrontEnd/PopUpModifierController.java
+++ gihFrontEnd/src/main/java/ma/uiass/eia/pds/gihFrontEnd/PopUpModifierController.java
@@ -5,8 +5,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import ma.uiass.eia.pds.gihBackEnd.model.DisponibiliteLit;
+import ma.uiass.eia.pds.gihBackEnd.model.Espace;
+import ma.uiass.eia.pds.gihBackEnd.model.EtatLit;
 import ma.uiass.eia.pds.gihBackEnd.model.Lit;
 import okhttp3.Call;
+import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
 import java.net.URL;
@@ -14,13 +18,13 @@ import java.util.ResourceBundle;
 
 public class PopUpModifierController implements Initializable {
     @FXML
-    private ComboBox<?> cboxDisponibilite;
+    private ComboBox<DisponibiliteLit> cboxDisponibilite;
 
     @FXML
-    private ComboBox<?> cboxEspace;
+    private ComboBox<Espace> cboxEspace;
 
     @FXML
-    private ComboBox<?> cboxEtat;
+    private ComboBox<EtatLit> cboxEtat;
 
     @FXML
     private Label lblBatiment;
@@ -34,6 +38,7 @@ public class PopUpModifierController implements Initializable {
     @FXML
     private Label lblType;
 
+    OkHttpClient okHttpClient = new OkHttpClient();
     public void getLitUpdate(Lit lit){
         lblMarque.setText(lit.getMarque().toString());
         lblType.setText(lit.getTypeLit().toString());
