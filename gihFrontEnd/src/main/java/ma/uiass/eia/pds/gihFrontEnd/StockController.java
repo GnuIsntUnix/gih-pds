13716af578f991f55d38689bf4a2adfa11f18b09
imane <im.elkasiti@gmail.com>
2023-03-27 16:18:37 +0000

    test

--- gihFrontEnd/src/main/java/ma/uiass/eia/pds/gihFrontEnd/StockController.java
+++ gihFrontEnd/src/main/java/ma/uiass/eia/pds/gihFrontEnd/StockController.java
@@ -1,5 +1,39 @@
 package ma.uiass.eia.pds.gihFrontEnd;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
 public class StockController {
+    @FXML
+    private Button affecter;
+
+    @FXML
+    private Button ajouter;
+
+    @FXML
+    private TableColumn<?, ?> date;
+
+    @FXML
+    private TableColumn<?, ?> etatDemande;
+
+    @FXML
+    private TableColumn<?, ?> marque;
+
+    @FXML
+    private Button modifier;
+
+    @FXML
+    private TableColumn<?, ?> numDemande;
+
+    @FXML
+    private TableColumn<?, ?> qtte;
+
+    @FXML
+    private Button quitter;
+
+    @FXML
+    private Button supprimer;
+
 
 }
