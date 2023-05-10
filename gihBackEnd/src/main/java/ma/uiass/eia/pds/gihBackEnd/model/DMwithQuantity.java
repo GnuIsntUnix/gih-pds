21419a8768d3e875f364bd37bbf6ae08dfec9703
hamza <hamza27122002@gmail.com>
2023-05-10 22:55:39 +0100

    v9

--- gihBackEnd/src/main/java/ma/uiass/eia/pds/gihBackEnd/model/DMwithQuantity.java
+++ gihBackEnd/src/main/java/ma/uiass/eia/pds/gihBackEnd/model/DMwithQuantity.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "TDMwithQuatity")
+@Table(name = "TDMwithQuantity")
 public class DMwithQuantity extends DM {
 
     @Column(name = "Quantité")
